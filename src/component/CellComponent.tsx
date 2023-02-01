f1668d289c16e195bf3be90becaad5c0c7309ee2
gnomboltun <leevaa0@inbox.ru>
2023-02-01 12:23:29 +0300

    Methods to add figures on board

--- src/component/CellComponent.tsx
+++ src/component/CellComponent.tsx
@@ -1,20 +1,21 @@
-import React, { FC } from 'react'
-import { Cell } from '../models/Cell'
-import { motion } from 'framer-motion';
+import React, { FC } from "react";
+import { Cell } from "../models/Cell";
+import { motion } from "framer-motion";
 
 interface CellProps {
-    cell: Cell;
+  cell: Cell;
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({ cell }) => {
   return (
-    <motion.div 
-    whileHover={{ scale: [null, 1.2, 1.1] }}
-    transition={{ duration: 0.3 }}
-    className={['cell', cell.color].join(' ')}>
-
+    <motion.div
+      whileHover={{ scale: [null, 1.2, 1.1] }}
+      transition={{ duration: 0.3 }}
+      className={["cell", cell.color].join(" ")}
+    >
+      {cell.figure?.logo && <img src={cell.figure.logo} alt="F"/>}
     </motion.div>
-  )
-}
+  );
+};
 
-export default CellComponent
+export default CellComponent;
--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,21 +1,71 @@
-import { Colors } from './Colors';
-import { Cell } from './Cell';
+import { Rook } from "./Figures/Rook";
+import { Bishop } from "./Figures/Bishop";
+import { Knight } from "./Figures/Knight";
+import { King } from "./Figures/King";
+import { Colors } from "./Colors";
+import { Cell } from "./Cell";
+import { Queen } from "./Figures/Queen";
+import { Pawn } from "./Figures/Pawn";
 export class Board {
-    cells: Cell[][] = [];
+  cells: Cell[][] = [];
 
-    public initCells() {
-        for (let i = 0; i < 8; i++) {
-            const row: Cell[] = []
-            for (let j = 0; j < 8; j++) {
-                if ((i+ j) % 2 !== 0) {
-                    row.push(new Cell(this, j, i, Colors.BLACK, null)) //black cells
-                } else {
-                    row.push(new Cell(this, j, i, Colors.WHITE, null)) //white cells
-                }
-                
-            }
-            this.cells.push(row);
-            
+  public initCells() {
+    for (let i = 0; i < 8; i++) {
+      const row: Cell[] = [];
+      for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 !== 0) {
+          row.push(new Cell(this, j, i, Colors.BLACK, null)); //black cells
+        } else {
+          row.push(new Cell(this, j, i, Colors.WHITE, null)); //white cells
         }
+      }
+      this.cells.push(row);
     }
+  }
+
+  public getCell(x: number, y: number) {
+    return this.cells[y][x];
+  }
+
+  private addPawns() {
+    for (let i = 0; i < 8; i++) {
+      new Pawn(Colors.BLACK, this.getCell(i, 1));
+      new Pawn(Colors.WHITE, this.getCell(i, 6));
+    }
+  }
+  private addBishops() {
+    new Bishop(Colors.BLACK, this.getCell(2, 0));
+    new Bishop(Colors.BLACK, this.getCell(5, 0));
+    new Bishop(Colors.WHITE, this.getCell(2, 7));
+    new Bishop(Colors.WHITE, this.getCell(5, 7));
+  }
+  private addRooks() {
+    new Rook(Colors.BLACK, this.getCell(0, 0));
+    new Rook(Colors.BLACK, this.getCell(7, 0));
+    new Rook(Colors.WHITE, this.getCell(0, 7));
+    new Rook(Colors.WHITE, this.getCell(7, 7));
+  }
+  private addKnights() {
+    new Knight(Colors.BLACK, this.getCell(1, 0));
+    new Knight(Colors.BLACK, this.getCell(6, 0));
+    new Knight(Colors.WHITE, this.getCell(1, 7));
+    new Knight(Colors.WHITE, this.getCell(6, 7));
+  }
+  private addQueens() {
+    new Queen(Colors.BLACK, this.getCell(3, 0));
+    new Queen(Colors.WHITE, this.getCell(3, 7));
+  }
+  private addKings() {
+    new King(Colors.BLACK, this.getCell(4, 0));
+    new King(Colors.WHITE, this.getCell(4, 7));
+  }
+
+  public addFigures() {
+    this.addPawns();
+    this.addBishops();
+    this.addRooks();
+    this.addKnights();
+    this.addQueens();
+    this.addKings();
+  }
 }
